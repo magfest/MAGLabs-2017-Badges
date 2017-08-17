2cce8dc73ff3b4739af673bf991f5cc5db0063b1
cnlohr <lohr85@gmail.com>
2017-08-16 21:07:04 -0400

    Add the rest of the esp82xx firmware setup

---
+++ xxfirmware/user/user_main.c
@@ -0,0 +1,128 @@
+//Copyright 2015 <>< Charles Lohr, see LICENSE file.
+
+#include "mem.h"
+#include "c_types.h"
+#include "user_interface.h"
+#include "ets_sys.h"
+#include "uart.h"
+#include "osapi.h"
+#include "espconn.h"
+#include "esp82xxutil.h"
+#include "commonservices.h"
+#include "vars.h"
+#include <mdns.h>
+
+#define procTaskPrio        0
+#define procTaskQueueLen    1
+
+static volatile os_timer_t some_timer;
+static struct espconn *pUdpServer;
+usr_conf_t * UsrCfg = (usr_conf_t*)(SETTINGS.UserData);
+
+//int ICACHE_FLASH_ATTR StartMDNS();
+
+
+void user_rf_pre_init(void) { /*nothing*/ }
+
+//Tasks that happen all the time.
+
+os_event_t    procTaskQueue[procTaskQueueLen];
+
+static void ICACHE_FLASH_ATTR procTask(os_event_t *events)
+{
+	CSTick( 0 );
+	system_os_post(procTaskPrio, 0, 0 );
+}
+
+//Timer event.
+static void ICACHE_FLASH_ATTR myTimer(void *arg)
+{
+	static	int frameno;
+	int i;
+	frameno++;
+	uint8_t leds[12];
+	for( i = 0; i < 4; i++ )
+	{
+		leds[i*3+0] = frameno;
+		leds[i*3+1] = frameno;
+		leds[i*3+2] = frameno;
+	}
+
+	ws2812_push( leds, 4);
+	CSTick( 1 ); // Send a one to uart
+}
+
+
+//Called when new packet comes in.
+static void ICACHE_FLASH_ATTR
+udpserver_recv(void *arg, char *pusrdata, unsigned short len)
+{
+	struct espconn *pespconn = (struct espconn *)arg;
+
+	uart0_sendStr("X");
+}
+
+void ICACHE_FLASH_ATTR charrx( uint8_t c )
+{
+	//Called from UART.
+}
+
+void user_init(void)
+{
+	uart_init(BIT_RATE_115200, BIT_RATE_115200);
+
+	uart0_sendStr("\r\nesp82XX Web-GUI\r\n" VERSSTR "\b");
+
+//Uncomment this to force a system restore.
+//	system_restore();
+
+	CSSettingsLoad( 0 );
+	CSPreInit();
+
+    pUdpServer = (struct espconn *)os_zalloc(sizeof(struct espconn));
+	ets_memset( pUdpServer, 0, sizeof( struct espconn ) );
+	espconn_create( pUdpServer );
+	pUdpServer->type = ESPCONN_UDP;
+	pUdpServer->proto.udp = (esp_udp *)os_zalloc(sizeof(esp_udp));
+	pUdpServer->proto.udp->local_port = COM_PORT;
+	espconn_regist_recvcb(pUdpServer, udpserver_recv);
+
+	if( espconn_create( pUdpServer ) )
+	{
+		while(1) { uart0_sendStr( "\r\nFAULT\r\n" ); }
+	}
+
+	CSInit();
+
+	SetServiceName( "labs17" );
+	AddMDNSName(    "esp82xx" );
+	AddMDNSName(    "labs17" );
+	AddMDNSService( "_http._tcp",    "An ESP82XX Webserver", WEB_PORT );
+	AddMDNSService( "_espcom._udp",  "ESP82XX Comunication", COM_PORT );
+	AddMDNSService( "_esp82xx._udp", "ESP82XX Backend",      BACKEND_PORT );
+
+	//Add a process
+	system_os_task(procTask, procTaskPrio, procTaskQueue, procTaskQueueLen);
+
+	//Timer example
+	os_timer_disarm(&some_timer);
+	os_timer_setfn(&some_timer, (os_timer_func_t *)myTimer, NULL);
+	os_timer_arm(&some_timer, 100, 1);
+
+	printf( "Boot Ok.\n" );
+
+	ws2812_init();
+
+//	wifi_set_sleep_type(LIGHT_SLEEP_T);
+//	wifi_fpm_set_sleep_type(LIGHT_SLEEP_T);
+
+	system_os_post(procTaskPrio, 0, 0 );
+}
+
+
+//There is no code in this project that will cause reboots if interrupts are disabled.
+void EnterCritical() { }
+
+void ExitCritical() { }
+
+
